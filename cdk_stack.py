77fb8db82eab4e33c895abd47fbd2024a4448cb7
Charlie O'Hara <charlie.b.ohara@gmail.com>
2023-02-27 15:56:45 -0800

    refactor into prod and dev envs, simplify cdk setup

--- cdk_stack.py
+++ cdk_stack.py
@@ -8,23 +8,25 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_sqs,
     Stack,
-    Duration
+    Duration,
+    App
 )
 
-class ApiLambdaStack(Stack):
+
+class SlackAppStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         aws_account = os.environ["CDK_DEFAULT_ACCOUNT"]
+        env = os.environ["ENV"]
 
         # Creating IAM role for Lambda function
         lambda_role = iam.Role(
             self, 
-            'BoltLambdaRole',
+            'SlackAppLambdaRole',
             assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
-            role_name='bolt_python_lambda_invocation',
-            description='Bolt Python basic role'
+            role_name=f'{env}-slack-app-lambda-role'
         )
 
         # Attach policies to the IAM role
@@ -51,11 +53,11 @@ class ApiLambdaStack(Stack):
         )
 
         # Creating Lambda function that will be triggered by Lambda function URL
-        lambda_function = python.PythonFunction(
+        lambda_listener_function = python.PythonFunction(
             self,
-            'LambdaFunction',
+            f'{env}-lambda-listener-function',
             runtime=_lambda.Runtime.PYTHON_3_9,
-            entry='lambda',
+            entry='lambda_listener',
             index='lambda_handler.py',
             handler='handler',
             environment={
@@ -73,21 +75,21 @@ class ApiLambdaStack(Stack):
         )
 
         # Create function URL
-        function_url = lambda_function.add_function_url(
+        function_url = lambda_listener_function.add_function_url(
             auth_type=_lambda.FunctionUrlAuthType.NONE,
         )
 
         # Create dead letter queue for safekeeping
         dead_letter_queue = aws_sqs.Queue(
             self,
-            'DeadLetterQueue',
+            f'{env}-lambda-listener-dead-letter-queue',
             retention_period=Duration.days(14)
         )
 
         # Send messages to sqs for processing
         queue = aws_sqs.Queue(
             self,
-            'Queue',
+            f'{env}-lambda-listener-queue',
             retention_period=Duration.days(14),
             visibility_timeout=Duration.seconds(300),
             dead_letter_queue=aws_sqs.DeadLetterQueue(
@@ -97,34 +99,33 @@ class ApiLambdaStack(Stack):
         )
 
         # Grant Lambda to send message to SQS
-        queue.grant_send_messages(lambda_function)
+        queue.grant_send_messages(lambda_listener_function)
 
-        # Create bucket
-        # Create S3 bucket for installation credentials
-        # slack_install_bucket = s3.Bucket(
-        #     self,
-        #     "SlackInstallationsBucket",
-        #     bucket_name=f"slack-installations-s3-{aws_account}",
-        # )
-        
-        # Create S3 bucket for state variables during OAuth flow
-        # slack_state_store_bucket = s3.Bucket(
-        #     self,
-        #     "SlackStateStoreBucket",
-        #     bucket_name=f"slack-state-store-s3-{aws_account}",
-        # )
+#        # Create S3 bucket for installation credentials
+#        slack_install_bucket = s3.Bucket(
+#            self,
+#            "SlackInstallationsBucket",
+#            bucket_name=f"{env}-slack-installations-s3-{aws_account}",
+#        )
+#        
+#        # Create S3 bucket for state variables during OAuth flow
+#        slack_state_store_bucket = s3.Bucket(
+#            self,
+#            "SlackStateStoreBucket",
+#            bucket_name=f"{env}-slack-state-store-s3-{aws_account}",
+#        )
 
         # Import existing buckets
         slack_install_bucket = s3.Bucket.from_bucket_name(
             self,
-            "SlackInstallationsBucket",
-            bucket_name=f"slack-installations-s3-{aws_account}",
+            f"{env}-slack-installations-s3-{aws_account}",
+            bucket_name=f"{env}-slack-installations-s3-{aws_account}",
         )
 
         slack_state_store_bucket = s3.Bucket.from_bucket_name(
             self,
-            "SlackStateStoreBucket",
-            bucket_name=f"slack-state-store-s3-{aws_account}",
+            f"{env}-slack-state-store-s3-{aws_account}",
+            bucket_name=f"{env}-slack-state-store-s3-{aws_account}",
         )
 
         # Define the IAM policy statement
@@ -145,27 +146,10 @@ class ApiLambdaStack(Stack):
         # Attach the policy to the Lambda role so it can access S3
         lambda_role.add_to_policy(s3_policy)
 
-#        # Create the API GW service role with permissions to call SQS
-#        rest_api_role = iam.Role(
-#            self,
-#            "RestAPIRole",
-#            assumed_by=iam.ServicePrincipal("apigateway.amazonaws.com"),
-#            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambda_FullAccess")]
-#        )
-#
-#        # Create an API GW Rest API
-#        base_api = apigw.RestApi(
-#            self,
-#            'ApiGW',
-#            rest_api_name='slack',
-#        )
-#
-#        # Create a resource for the base API
-#        api_resource = base_api.root.add_resource('events')
-#
-#        # Add POST method for slack to POST events to
-#        post_method = api_resource.add_method(
-#            "POST",
-#            apigw.LambdaIntegration(lambda_function),
-#        )
-#
+
+app = App()
+SlackAppStack(
+    app, 
+    f'{os.environ["ENV"]}-{os.environ["NAME"]}'
+)
+app.synth()
--- deploy.py
+++ deploy.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 
 parser = argparse.ArgumentParser()
-#parser.add_argument('--config', dest='config')
+parser.add_argument('--config', dest='config')
 args = parser.parse_args()
 
 load_dotenv(dotenv_path='.env')
-#load_dotenv(dotenv_path=args.config)
+load_dotenv(dotenv_path=args.config)
 subprocess.call(['cdk', 'deploy', '-v'], cwd='.')
