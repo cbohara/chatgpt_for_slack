5e36a6f29705674695c4439768c334271eb46233
Charlie O'Hara <charlie.b.ohara@gmail.com>
2023-03-02 17:37:23 -0800

    update lambda listener to use new openai api and see if it performs better and doesnt require lambda lazy approach = black magic

--- lambda_listener/lambda_handler.py
+++ lambda_listener/lambda_handler.py
@@ -3,13 +3,10 @@ import os
 import json
 import logging
 import boto3
+import openai
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 from slack_bolt.adapter.aws_lambda.lambda_s3_oauth_flow import LambdaS3OAuthFlow
-
-from langchain import OpenAI, LLMChain, PromptTemplate
-from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
-
 
 SlackRequestHandler.clear_all_log_handlers()
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
@@ -23,42 +20,31 @@ private_chats = dict()
 public_chats = dict()
 
 
-def get_prompt():
-    template = """Assistant is a large language model trained by OpenAI.
-
-    Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-    Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-    Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-    {history}
-    Human: {human_input}
-    Assistant:"""
-    return PromptTemplate(
-        input_variables=["history", "human_input"], 
-        template=template
-    )
-
+#def respond_to_slack_within_3_seconds(body, ack):
+#    ack("Let slack know app is processing request")
 
 def start_chat():
-    return LLMChain(
-        llm=OpenAI(
-            temperature=0,
-            max_retries=0
-        ), 
-        prompt=get_prompt(), 
-        verbose=True, 
-        memory=ConversationalBufferWindowMemory(k=10),
+    return [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+
+def get_openai_response(chat):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat
     )
+    return response["choices"][0]["message"]["content"]
 
 
+def trim_chat(chat):
+    if len(chat) == 5:
+        del(chat[1:3])
+        return chat
+    return chat
+    
 
-def respond_to_slack_within_3_seconds(body, ack):
-    ack("Let slack know app is processing request")
-
-
-# need to figure out how to avoid duplicate events
+@app.event("app_mention")
 def app_mention_event(event, say, logger):
     team_id = event.get("team")
     channel_id = event.get("channel")
@@ -76,16 +62,25 @@ def app_mention_event(event, say, logger):
         public_chats[public_chat_id] = chat
 
     user_input = event.get("text")
-    openai_response = chat.predict(human_input=user_input)
+    chat.append(
+        {"role": "user", "content": user_input}
+    )
+    openai_response = get_openai_response(chat)
     say(openai_response, thread_ts=thread_ts)
+    chat.append(
+        {"role": "assistant", "content": openai_response}
+    )
+    chat = trim_chat(chat)
+    public_chats[public_chat_id] = chat
 
 
-app.event("app_mention")(
-    ack=respond_to_slack_within_3_seconds,
-    lazy=[app_mention_event]
-)
+#app.event("app_mention")(
+#    ack=respond_to_slack_within_3_seconds,
+#    lazy=[app_mention_event]
+#)
 
 
+@app.event("message")
 def message_event(event, say, logger):
     channel_type = event.get("channel_type")
     if channel_type == "im":
@@ -103,32 +98,41 @@ def message_event(event, say, logger):
             private_chats[private_chat_id] = chat
             
         user_input = event.get("text")
-        openai_response = chat.predict(human_input=user_input)
+        chat.append(
+            {"role": "user", "content": user_input}
+        )
+        openai_response = get_openai_response(chat)
         say(openai_response, channel=channel_id)
+        chat.append(
+            {"role": "assistant", "content": openai_response}
+        )
+        chat = trim_chat(chat)
+        private_chats[private_chat_id] = chat
 
 
-app.event("message")(
-    ack=respond_to_slack_within_3_seconds,
-    lazy=[message_event]
-)
+
+#app.event("message")(
+#    ack=respond_to_slack_within_3_seconds,
+#    lazy=[message_event]
+#)
 
 
-def get_sqs_message(slack_event):
-    """
-    Pass slack event into SQS message for downstream processing
-    """
-    return json.dumps(slack_event)
-
-
-def send_sqs_message(sqs_message):
-    """
-    Send SQS message to SQS queue
-    """
-    sqs = boto3.client('sqs')
-    sqs.send_message(
-        QueueUrl="https://sqs.us-east-1.amazonaws.com/846078712375/ApiLambdaStack-Queue4A7E3555-5oHbpDACP4F4",
-        MessageBody=sqs_message
-    )
+#def get_sqs_message(slack_event):
+#    """
+#    Pass slack event into SQS message for downstream processing
+#    """
+#    return json.dumps(slack_event)
+#
+#
+#def send_sqs_message(sqs_message):
+#    """
+#    Send SQS message to SQS queue
+#    """
+#    sqs = boto3.client('sqs')
+#    sqs.send_message(
+#        QueueUrl="https://sqs.us-east-1.amazonaws.com/846078712375/ApiLambdaStack-Queue4A7E3555-5oHbpDACP4F4",
+#        MessageBody=sqs_message
+#    )
 
 
 def challenge_response(challenge):
